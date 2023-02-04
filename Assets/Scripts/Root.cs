83ce91257c09ae018771dadcf2ac56176a6b9847
Kyle <kyled073@gmail.com>
2023-02-04 04:04:34 +0000

    Root Branches
    Branches towards a goal but only from a certain position, need to redo so it does it from any given position :)

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -7,20 +7,24 @@ public class Root : MonoBehaviour
     public Root parent;
     private LineRenderer lines;
     public GameObject rootPrefab;
-    private float mutationRate = 0.5f;
+    private float mutationRate = 0.8f;
     public List<Root> children = new List<Root>();
-    public Vector3 pointA = new Vector3(0,0,0);
-    public Vector3 pointB = new Vector3(0,-1,0);
+    public Vector3 pointA;
+    public Vector3 pointB;
     public bool isDead = false;
     private int count = 0;
+    public Vector3 goal = new Vector3(0, 0, 0);
+    private float focus = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
         lines = GetComponent<LineRenderer>();
+        pointA = transform.position;
+        pointB = transform.position + Vector3.down;
         Draw();
 
-        if(Random.Range(0.0f, 1.0f) > 0.75f){
+        if(Random.Range(0.0f, 1.0f) > 10.0f){
             mutationRate = 1.1f;
         }
     }
@@ -39,24 +43,42 @@ public class Root : MonoBehaviour
     void Update()
     {
         if(count < 3){
-            doesBranch();
+            DoesBranch();
         }
     }
 
-    void doesBranch() {
-        if (Random.Range(0.0f, 1.0f) > mutationRate) {
+    void DoesBranch() {
+        if(Vector3.Distance(pointB, goal) < 3.0f){
+            mutationRate = 10.0f;
+        } else if (Random.Range(-1.0f, 1.0f) > mutationRate) {
             count++;
             createChild();
-            doesBranch();
+            DoesBranch();
         }
+    }
+
+    float DoStuff(float i) {
+        if (i >= 0) {
+            return Random.Range(-2.0f * focus, 2.0f);
+        } else {
+            return Random.Range(2.0f, 2.0f * focus);
+        }
+    }
+
+    Vector3 GetNewPosition() {
+        Vector3 direction = goal - pointB;
+
+        float X = DoStuff(direction.x);
+        float Y = DoStuff(direction.y);
+
+        return new Vector3(X, Y, 0);
     }
 
     void createChild() {
-        Vector3 newPoint = new Vector3(Random.Range(-2.0f,2.0f), Random.Range(-0.5f,-2.0f)) + pointB;
+        Vector3 newPoint = pointB + GetNewPosition();
         GameObject newRootRef = Instantiate(rootPrefab);
         Root newRoot = newRootRef.GetComponent<Root>();
         newRoot.SetPoints(pointB, newPoint);
-        newRoot.Draw();
         children.Add(newRoot);
     }
 }
