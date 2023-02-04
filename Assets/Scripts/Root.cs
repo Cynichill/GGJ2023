0d82dfd678468cb8c5aaab5f333d362de55894de
Kyle <kyled073@gmail.com>
2023-02-04 09:41:20 +0000

    Roots will not collide with self
    Need to implement same for branches

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -11,17 +11,20 @@ class Root : MonoBehaviour
     private float mutationRate = 0.4f;
     public List<Root> children = new List<Root>();
     public List<Vector3> positions = new List<Vector3>();
+    public MeshCollider collider;
     public bool isDead = false;
     private int count = 0;
     private Vector3 goal = new Vector3(5, -4, 0);
     private float focus = 0.4f;
+    private Mesh mesh;
+    public EdgeCollider2D edgeCollider;
 
     void Start()
     {
         lines = GetComponent<LineRenderer>();
-        lines.material.SetColor("_Color", new Color(225/255f,228/255f,196/255f, 1));
+        lines.material.SetColor("_Color", new Color(225/255f,228/255f,196/255f, 0.1f));
         positions.Add(transform.position);
-        positions.Add(transform.position);
+        positions.Add(transform.position + new Vector3(0.1f, 0 , 0));
         Draw();
         if(Random.Range(0.0f, 1.0f) > 10.0f){
             mutationRate = 1.1f;
@@ -29,19 +32,20 @@ class Root : MonoBehaviour
     }
 
     public void Draw() {
-        Debug.Log(lines);
         lines.positionCount = positions.Count;
         lines.SetPositions(positions.ToArray());
+        SetEdgeCollider();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Extend();
-        if(count < 1){
-            DoesBranch();
+        if(!isDead && Random.Range(0.0f, 1.0f) > 0.5f){
+            Extend();
         }
-        Debug.Log(goal);
+        if(count < 1){
+            //DoesBranch();
+        }
     }
 
     void SetRoot(Vector3 start, Vector3 end) {
@@ -49,13 +53,60 @@ class Root : MonoBehaviour
 
     void Extend() {
         if(Vector3.Distance(positions.Last(), goal) > 0.5f){
-            Vector3 newPoint = positions.Last() + GetNewPosition();
-            positions.Add(newPoint);
-            Debug.Log(positions.ToArray());
-            Draw();
+            Vector3 newPoint = new Vector3(0,0,0);
+
+            int attempts = 0;
+            bool create = false;
+
+            while(attempts < 3 && !create){
+                newPoint = positions.Last() + GetNewPosition();
+                //Debug.Log("Testing Path '" + newPoint + "'...");
+
+                if(IsClearPath(newPoint)){
+                    create = true;
+                    //Debug.Log("Clear Path: " + newPoint);
+                }
+                attempts++;
+            }
+
+            if(create){
+                //Debug.Log("Creating Point: " + newPoint);   
+                positions.Add(newPoint);
+                Draw();
+            } else {
+                isDead = true;
+            } 
         } else {
-            Debug.Log("found");
+            // trigger attack mode
         }
+    }
+
+    void GenerateMeshCollider() {
+        collider = GetComponent<MeshCollider>();
+
+        if(collider == null) {
+            collider.gameObject.AddComponent<MeshCollider>();
+        }
+
+        mesh = new Mesh();
+        lines.BakeMesh(mesh, Camera.main, false);
+        collider.sharedMesh = mesh;
+
+        foreach(var i in mesh.normals) {
+            Debug.Log(i);
+        }
+    }
+
+    void SetEdgeCollider() {
+        edgeCollider = GetComponent<EdgeCollider2D>();
+
+        List<Vector2> edges = new List<Vector2>(); 
+
+        foreach(Vector3 point in positions){
+            edges.Add(new Vector2(point.x, point.y));
+        }
+
+        edgeCollider.SetPoints(edges);
     }
 
     void DoesBranch() {
@@ -84,14 +135,46 @@ class Root : MonoBehaviour
 
         return new Vector3(X, Y, 0);
     }
+    
+    bool IsClearPath(Vector3 newPoint) {
+        Vector3 pointA = positions.Last() + 0.1f * (newPoint-positions.Last());// + newPoint * 0.1f;
+        Debug.Log("TESTING " + newPoint);
+        Debug.Log(pointA + " => " + newPoint + " = " + Vector3.Distance(pointA, newPoint));
+        Debug.Log(mesh);
+        Debug.DrawRay(pointA, newPoint-pointA, Color.green, 10000.0f, false);
+        if (Physics2D.Linecast(newPoint, pointA)){
+            Debug.Log(":::::Collision Detected::::: ");
+            Debug.Log(":::::Collision Detected::::: ");
+            Debug.Log(":::::Collision Detected::::: ");
+            return false;
+        } else {
+            return true;
+        }
+    }
 
     void createChild() {
         GameObject newRootRef = Instantiate(rootPrefab, positions.Last(), rootPrefab.transform.rotation);
         Root newRoot = newRootRef.GetComponent<Root>();
-        Vector3 newPoint = positions.Last() + GetNewPosition();
 
-        newRoot.SetRoot(positions.Last(), newPoint);
+        int attempts = 0;
+        bool create = false;
 
-        children.Add(newRoot);
+        Vector3 newPoint = new Vector3(0,0,0);
+
+        while(attempts < 3 && !create){
+            newPoint = positions.Last() + GetNewPosition();
+
+            if(IsClearPath(newPoint)){
+                create = true;
+                Debug.Log("Collision Detected: " + newPoint);
+            }
+            attempts++;
+        }
+
+        if(create) {
+            Debug.Log("New Child");
+            newRoot.SetRoot(positions.Last(), newPoint);
+            children.Add(newRoot);
+        }
     }
 }
