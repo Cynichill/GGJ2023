9da3b7554517549ae068684da3967dce1102d7b3
Kyle <kyled073@gmail.com>
2023-02-04 05:41:55 +0000

    Restructure
    Instead of segments, lines now redraw themselves and new branches are root objects

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -1,54 +1,64 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Root : MonoBehaviour
+class Root : MonoBehaviour
 {
     public Root parent;
     private LineRenderer lines;
     public GameObject rootPrefab;
     private float mutationRate = 0.8f;
     public List<Root> children = new List<Root>();
-    public Vector3 pointA;
-    public Vector3 pointB;
+    public List<Vector3> positions = new List<Vector3>();
     public bool isDead = false;
     private int count = 0;
-    public Vector3 goal = new Vector3(0, 0, 0);
+    private Vector3 goal = new Vector3(5, -4, 0);
     private float focus = 0.2f;
 
-    // Start is called before the first frame update
     void Start()
     {
         lines = GetComponent<LineRenderer>();
-        pointA = transform.position;
-        pointB = transform.position + Vector3.down;
+        positions.Add(transform.position);
+        positions.Add(transform.position);
         Draw();
-
         if(Random.Range(0.0f, 1.0f) > 10.0f){
             mutationRate = 1.1f;
         }
     }
 
     public void Draw() {
-        lines.positionCount = 2;
-        lines.SetPositions(new Vector3[2]{pointA, pointB});
-    }
-
-    public void SetPoints(Vector3 A, Vector3 B){
-        pointA = A;
-        pointB = B;
+        Debug.Log(lines);
+        lines.positionCount = positions.Count;
+        lines.SetPositions(positions.ToArray());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(count < 3){
+        Extend();
+        if(count < 1){
             DoesBranch();
+        }
+        Debug.Log(goal);
+    }
+
+    void SetRoot(Vector3 start, Vector3 end) {
+    }
+
+    void Extend() {
+        if(Vector3.Distance(positions.Last(), goal) > 0.5f){
+            Vector3 newPoint = positions.Last() + GetNewPosition();
+            positions.Add(newPoint);
+            Debug.Log(positions.ToArray());
+            Draw();
+        } else {
+            Debug.Log("found");
         }
     }
 
     void DoesBranch() {
-        if(Vector3.Distance(pointB, goal) < 3.0f){
+        if(Vector3.Distance(positions.Last(), goal) < 3.0f){
             mutationRate = 10.0f;
         } else if (Random.Range(-1.0f, 1.0f) > mutationRate) {
             count++;
@@ -61,12 +71,12 @@ public class Root : MonoBehaviour
         if (i >= 0) {
             return Random.Range(-2.0f * focus, 2.0f);
         } else {
-            return Random.Range(2.0f, 2.0f * focus);
+            return Random.Range(-2.0f, 2.0f * focus);
         }
     }
 
     Vector3 GetNewPosition() {
-        Vector3 direction = goal - pointB;
+        Vector3 direction = goal - positions.Last();
 
         float X = DoStuff(direction.x);
         float Y = DoStuff(direction.y);
@@ -75,10 +85,12 @@ public class Root : MonoBehaviour
     }
 
     void createChild() {
-        Vector3 newPoint = pointB + GetNewPosition();
-        GameObject newRootRef = Instantiate(rootPrefab);
+        GameObject newRootRef = Instantiate(rootPrefab, positions.Last(), rootPrefab.transform.rotation);
         Root newRoot = newRootRef.GetComponent<Root>();
-        newRoot.SetPoints(pointB, newPoint);
+        Vector3 newPoint = positions.Last() + GetNewPosition();
+
+        newRoot.SetRoot(positions.Last(), newPoint);
+
         children.Add(newRoot);
     }
 }
