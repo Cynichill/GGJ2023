6785d8ae60a26110c5b30f331d4dbdcccb3cd218
Fiona "Errolthree" Erelyn Freyja Hannah <77973580+FioDev@users.noreply.github.com>
2023-02-05 16:22:31 +0000

    Fixed vec3 into vec2

--- Assets/new Script Structure/Items/ItemAxe.cs
+++ Assets/new Script Structure/Items/ItemAxe.cs
@@ -24,8 +24,8 @@ public class ItemAxe : Item
         base.Use(usingPlayer);
         
         //Casts rays left or right dependant on wether or not 
-        Ray2D hitRay = new Ray2D(usingPlayer.GetGameObject().transform.position, usingPlayer.GetMovement());
-
+        Ray2D hitRay = new Ray2D(new Vector2(usingPlayer.GetGameObject().transform.position.x, usingPlayer.GetGameObject().transform.position.y), Vector2.ClampMagnitude(usingPlayer.GetMovement(), 1f));
+       
         RaycastHit2D[] hits = Physics2D.RaycastAll(hitRay.origin, hitRay.direction);
         Debug.DrawRay(hitRay.origin, hitRay.direction, Color.blue, 10000.0f, false);
 
@@ -44,7 +44,7 @@ public class ItemAxe : Item
         //Guard clause against no target
         if (target != null)
         {
-            target.Chop(usingPlayer.GetGameObject().transform.position + hitLocation);
+            target.Chop(hitRay.origin + hitLocation);
         }
 
          
